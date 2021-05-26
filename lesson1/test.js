a5520530a3a1bea392c1998ec251901f9f80c7af
murata <murata.naoya.infratop@gmail.com>
2021-05-26 21:04:59 +0900

    データのバインド

--- lesson1/test.js
+++ lesson1/test.js
@@ -1,13 +1,6 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!',
-    count: 0,
-    user: {
-      lastname: 'yamada',
-      firstname: 'taro',
-      prefecture: 'tokyo'
-    },
-    colors: ['red', 'Green', 'Blue']
+    message: 'Hello world',
   }
 })
