1d12c5fb11a8188a6489d7c92dead307440d0ece
murata <murata.naoya.infratop@gmail.com>
2021-05-26 20:52:13 +0900

    first commit

---
+++ lesson1/test.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!',
+    count: 0,
+    user: {
+      lastname: 'yamada',
+      firstname: 'taro',
+      prefecture: 'tokyo'
+    },
+    colors: ['red', 'Green', 'Blue']
+  }
+})
