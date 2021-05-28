2b38834944699b1978b015c3ab2dd25fb66a76c0
murata <murata.small.baseball@gmail.com>
2021-05-28 20:52:59 +0900

    算出プロパティ

--- lesson1/test.js
+++ lesson1/test.js
@@ -1,11 +1,23 @@
+const employees_array = [
+  {
+    name: '田中 太郎',
+    age: 25,
+    department: "人事部"
+  }
+]
+
 const vm = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
-    num: 1000000
+    employee_data: employees_array
   },
-  filters: {
-    filterNum: function(val){
-      return "¥¥" + val.toLocaleString()
+  computed: {
+    calculateBirthYear: function(){
+      return function(id){
+        return 2020-this.employee_data[id].age
+      }
     }
   }
 })
+
+
