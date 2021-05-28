59be3fb510e5598f8daa51504156b2e218c9b5bb
murata <murata.small.baseball@gmail.com>
2021-05-28 20:24:58 +0900

    cssに対するバインド

--- lesson1/test.js
+++ lesson1/test.js
@@ -1,19 +1,6 @@
-const employees = [
-  {
-  name : "田中 太郎",
-  age : 25,
-  department : "人事部"
-  },
-  {
-    name : "山田 花子",
-    age : 28,
-    department : "営業部"
-  }
-]
-
 const vm = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    employees_data: employees
+    color_data: 'color: blue'
   }
 })
