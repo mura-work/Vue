d37ca077f4c8d47ffaf9111d14e00f9f71ebe60f
murata <murata.small.baseball@gmail.com>
2021-05-28 20:17:43 +0900

    dataオブジェクトの書き方

--- lesson1/test.js
+++ lesson1/test.js
@@ -1,5 +1,19 @@
+const employees = [
+  {
+  name : "田中 太郎",
+  age : 25,
+  department : "人事部"
+  },
+  {
+    name : "山田 花子",
+    age : 28,
+    department : "営業部"
+  }
+]
+
 const vm = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
-    message: "message"}
+    employees_data: employees
+  }
 })
