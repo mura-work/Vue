73f24343b813b6f43d376ad54b69e809e8888d2f
murata <murata.small.baseball@gmail.com>
2021-05-28 21:20:32 +0900

    ディレクティブ

--- lesson1/test.js
+++ lesson1/test.js
@@ -1,23 +1,7 @@
-const employees_array = [
-  {
-    name: '田中 太郎',
-    age: 25,
-    department: "人事部"
-  }
-]
-
 const vm = new Vue({
   el: '#app',
   data: {
-    employee_data: employees_array
-  },
-  computed: {
-    calculateBirthYear: function(){
-      return function(id){
-        return 2020-this.employee_data[id].age
-      }
-    }
+    isIf: '3',
+    isShow: '3'
   }
 })
-
-
