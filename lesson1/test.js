48248898f0f736c910c6e36172aac9432b429ab2
murata <murata.small.baseball@gmail.com>
2021-05-28 19:25:37 +0900

    Vueインスタンスの作り方

--- lesson1/test.js
+++ lesson1/test.js
@@ -1,10 +1,5 @@
-Vue.component('hello-component', {
-  template: '<p>Hello</p>'
-})
-
-var app = new Vue({
-  el: '#app',
+const vm = new Vue({
+  el: "#app",
   data: {
-    message: "hello vue.js"
-  }
+    message: "message"}
 })
