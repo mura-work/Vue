f0aaed2dca0bb219df761bcef9a4ad4fb98044c2
murata <murata.small.baseball@gmail.com>
2021-05-30 20:25:51 +0900

    fix message

--- lesson1/test.js
+++ lesson1/test.js
@@ -1,35 +1,6 @@
-// 子コンポーネントの作成
-const counterButton = Vue.extend({
-  template: `<span>{{count}}回<button v-on:click="addCount">+1</button></span>`,
-  data: function(){
-    return {
-      count: 0
-    }
-  },
-  methods: {
-    addCount: function(){
-      this.count += 1,
-      this.$emit('increment')
-    }
-  }
-})
-
-// 親コンポーネントの作成
-new Vue({
-  el: '#counter-anchor',
-  components: {
-    'parent-count-button': counterButton
-  },
+const vm = new Vue({
+  el: "#app",
   data: {
-    total: 0,
-    counters: [
-      {name: 'Aのボタン'},
-      {name: 'Bのボタン'}
-    ]
-  },
-  methods: {
-    plusCount: function(){
-      this.total += 1
-    }
+    message: "hello world"
   }
 })
